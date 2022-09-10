78cd498dce71d985fb171381e5c36ed2e0812de9
Sebastián Suárez <sebastian.suarezr@udea.edu.co>
2022-09-10 16:35:47 -0500

    Fan and door functionalities added

--- things/Domotica_esp32/Domotica_esp32.ino
+++ things/Domotica_esp32/Domotica_esp32.ino
@@ -1,54 +1,49 @@
 
-/* 
--Temperature sensor
--Automatic (using a photoresistor and a PIR sensor) and Manual controlled lights
--
--
--
+/*
 IoT Course
 Universidad de Antioquia
 */
 
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Servo.h>
 
-#include <WiFi.h> //Library for Wifi connection (included with board info)
-#include <PubSubClient.h> // Library for MQTT connection (by Nick O'Leary)
-#include <OneWire.h> // by Paul Stoffregen
-#include <DallasTemperature.h> // by Miles Burton
-
-#include "config.h"  // network  SSID and password in this file
-#include "MQTT.hpp" // MQTT Broker IP in this file
-#include "ESP32_Utils.hpp" // WIFI connection
-#include "ESP32_Utils_MQTT.hpp" // MQTT connection
+#include "config.h"
+#include "MQTT.hpp"
+#include "ESP32_Utils.hpp"
+#include "ESP32_Utils_MQTT.hpp"
 
 unsigned long t_light = 0;
 unsigned long t_temp = 0;
 
-//Sensors pins
 int motionSensor = 15;
 int lightSensor = 12;
-const int oneWireBus = 4; //Temp sensor
+int led = 17;
+int fan = 14;
+Servo myservo;
+const int oneWireBus = 4;
 
-int led = 17; // light pin
-
-//light state variables
+// Things States
 int light_state = 0;
 int automatic_light = 0;
-
-// Values captured by sensors
-int motionSensorValue = digitalRead(motionSensor);
-int lightSensorValue = digitalRead(lightSensor);
+int fan_state = 0;
+int automatic_fan = 0;
 
 char msg[50];
 
-//Set the onewire interface
+// Set the onewire interface
 OneWire oneWire(oneWireBus);
 
-//Set the temp sensor to use the onewire interface
-DallasTemperature sensors (&oneWire);
+// Set the temp sensor to use the onewire interface
+DallasTemperature sensors(&oneWire);
 
-void setup() {
-  pinMode(led, OUTPUT);   
-  digitalWrite(led, LOW); 
+void setup()
+{
+  pinMode(led, OUTPUT);   // BUILTIN_LED pin as an output
+  digitalWrite(led, LOW); // Light initial state is OFF
+  myservo.attach(13);     // attaches the servo on pin 13 to the servo object
 
   // Set the serial monitor baud rate
   Serial.begin(115200);
@@ -60,6 +55,7 @@ void setup() {
   pinMode(motionSensor, INPUT);
   pinMode(lightSensor, INPUT);
   pinMode(led, OUTPUT);
+  pinMode(fan, OUTPUT);
 
   Serial.println("Starting ...");
   digitalWrite(led, LOW);
@@ -73,17 +69,15 @@ void setup() {
   }
   Serial.println("Ready");
   digitalWrite(led, LOW);
-
-
 }
 
-
-void loop() {
+void loop()
+{
 
   HandleMqtt();
 
-  
-  if (millis() > t_temp + 500) {
+  if (millis() > t_temp + 5000)
+  {
 
     // Read the temperature value
     sensors.requestTemperatures();
@@ -96,37 +90,69 @@ void loop() {
     Serial.print(temperatureC);
     Serial.println("°C");
 
+    if (automatic_fan == 1)
+    {
+
+      if (temperatureC >= 25)
+      {
+        digitalWrite(fan, HIGH);
+        if (fan_state == 0)
+        {
+          PublisMqttString("home/fan/0", "1");
+        }
+
+        fan_state = 1;
+      }
+      else
+      {
+        digitalWrite(fan, LOW);
+        if (fan_state == 1)
+        {
+          PublisMqttString("home/fan/0", "0");
+        }
+
+        fan_state = 0;
+      }
+    }
+
     // Send temperature value to the MQTT server
     // Device ID for temperature : 2
-    snprintf (msg, 50, "2,%.2f", temperatureC);
+    snprintf(msg, 50, " % .2f", temperatureC);
     Serial.print("Publish message: ");
     Serial.println(msg);
-    PublisMqttString("home", msg);
-    
-    t_temp = millis();
+    PublisMqttString("home/temp", msg);
 
+    t_temp = millis();
   }
 
-  if (automatic_light == 1) {
-    if (light_state == 1 && millis() > t_light + 5000) {
-      if (motionSensorValue == HIGH) {
+  int motionSensorValue = digitalRead(motionSensor);
+  int lightSensorValue = digitalRead(lightSensor);
+  // Serial.println(lightSensorValue);
+
+  if (automatic_light == 1)
+  {
+    if (light_state == 1 && millis() > t_light + 5000)
+    {
+      if (motionSensorValue == HIGH)
+      {
         t_light = millis();
       }
-      else {
+      else
+      {
         light_state = 0;
         digitalWrite(led, LOW);
-        PublisMqttString("home/light", "0");
+        PublisMqttString("home/light/0", "0");
       }
-
     }
-    else {
-      if (motionSensorValue == HIGH  && lightSensorValue == LOW) {
+    else
+    {
+      if (motionSensorValue == HIGH && lightSensorValue == LOW)
+      {
         light_state = 1;
         digitalWrite(led, HIGH);
-        PublisMqttString("home/light", "1");
+        PublisMqttString("home/light/0", "1");
         t_light = millis();
       }
-
     }
   }
 }
--- things/Domotica_esp32/MQTT.hpp
+++ things/Domotica_esp32/MQTT.hpp
@@ -1,23 +1,29 @@
 #include <string.h>
 
-const char* MQTT_BROKER_ADRESS = "192.168.25.125";
+const char *MQTT_BROKER_ADRESS = "192.168.25.241";
 const uint16_t MQTT_PORT = 1883;
-const char* MQTT_CLIENT_NAME = "ESPClient_1";
+const char *MQTT_CLIENT_NAME = "ESPClient_1";
 
 // Topics
-const char* DEVICE_TYPE = "Light";
-const char* DEVICE_ID = "0";
-#define RELAY 17
+const char *DEVICE_TYPE = "Light";
+const char *DEVICE_ID = "0";
 
+extern int led;
 extern int light;
 extern int automatic_light;
+extern Servo myservo;
+extern int fan;
+extern int fan_state;
+extern int automatic_fan;
+
+int pos = 0;
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 
 void SuscribeMqtt()
 {
-  mqttClient.subscribe("Light/#");
+  mqttClient.subscribe("room/#");
 }
 
 String payload;
@@ -25,72 +31,132 @@ void PublisMqtt(unsigned long data)
 {
   payload = "";
   payload = String(data);
-  mqttClient.publish("hello/world", (char*)payload.c_str());
+  mqttClient.publish("hello/world", (char *)payload.c_str());
 }
 
-void PublisMqttString(char* topic, char* msg)
+void PublisMqttString(char *topic, char *msg)
 {
   mqttClient.publish(topic, msg);
 }
 
 String content = "";
-void OnMqttReceived(char* topic, byte* payload, unsigned int length)
+void OnMqttReceived(char *topic, byte *payload, unsigned int length)
 {
 
   Serial.print("Received on ");
   Serial.print(topic);
   Serial.print(": ");
   content = "";
-  for (size_t i = 0; i < length; i++) {
+  for (size_t i = 0; i < length; i++)
+  {
     content.concat((char)payload[i]);
   }
   Serial.print(content);
   Serial.println();
 
-  if (strcmp(topic, "Light/automatic")) {
-    Serial.print("message recived on Light/automatic: ");
+  if (strcmp(topic, "room/light/0/automatic") == 0)
+  {
+    Serial.print("message recived on room/light/0/automatic: ");
     Serial.println((char)payload[0]);
 
-    if ((char)payload[0] == '1') {
+    if ((char)payload[0] == '1')
+    {
       automatic_light = 1;
       Serial.print("Automatic mode enabled: ");
       Serial.println(automatic_light);
-
     }
-    else {
+    else
+    {
       automatic_light = 0;
-      digitalWrite(RELAY, HIGH);
+      digitalWrite(led, LOW);
       Serial.print("Automatic mode desabled: ");
       Serial.println(automatic_light);
     }
   }
-  else if (strcmp(topic, "Light/0")) {
+  else if (strcmp(topic, "room/light/0") == 0)
+  {
     automatic_light = 0;
-    Serial.print("message recived on Light/0: ");
+    Serial.print("message recived on room/light/0: ");
     Serial.println((char)payload[0]);
-    
-    if ((char)payload[0] == '1') {
+
+    if ((char)payload[0] == '1')
+    {
       light = 1;
       Serial.println("recived signal to turn on light ");
-      digitalWrite(RELAY, HIGH);
+
+      digitalWrite(led, HIGH);
+      PublisMqttString("home/light/0", "1");
     }
-    else if ((char)payload[0] == '0') {
+    else if ((char)payload[0] == '0')
+    {
       light = 0;
       Serial.println("recived signal to turn off light ");
-      digitalWrite(RELAY, LOW);
+      digitalWrite(led, LOW);
+      PublisMqttString("home/light/0", "0");
     }
-    else if ((char)payload[0] == '2') {
+    else if ((char)payload[0] == '2')
+    {
 
       automatic_light = 0;
-      // Toggle light
-      if (light == 1) {
+      if (light == 1)
+      {
         light = 0;
-        digitalWrite(RELAY, LOW);
+        digitalWrite(led, LOW);
       }
-      else {
+      else
+      {
         light = 1;
-        digitalWrite(RELAY, HIGH);
+        digitalWrite(led, HIGH);
       }
+    }
+  }
+  else if (strcmp(topic, "room/fan/0") == 0)
+  {
+    if ((char)payload[0] == '1')
+    {
+      fan_state = 1;
+      Serial.println("recived signal to turn on the fan ");
+      digitalWrite(fan, HIGH);
+      PublisMqttString("home/fan/0", "1");
+    }
+    else if ((char)payload[0] == '0')
+    {
+      fan_state = 0;
+      Serial.println("recived signal to turn off the fan ");
+      digitalWrite(fan, LOW);
+      PublisMqttString("home/fan/0", "0");
+    }
+  }
+  else if (strcmp(topic, "room/fan/0/automatic") == 0)
+  {
+    if ((char)payload[0] == '1')
+    {
+      automatic_fan = 1;
+      Serial.print("recived signal to turn on the automatic behavior of fan: ");
+      Serial.println(automatic_fan);
+      digitalWrite(fan, LOW);
+    }
+    else if ((char)payload[0] == '0')
+    {
+      automatic_fan = 0;
+      Serial.print("recived signal to turn off the automatic behavior of fan: ");
+      Serial.println(automatic_fan);
+      digitalWrite(fan, LOW);
+    }
+  }
+  else if (strcmp(topic, "room/door/0") == 0)
+  {
+    if ((char)payload[0] == '1')
+    {
+      Serial.println("recived signal to open the door ");
+      PublisMqttString("home/door/0", "1");
+      myservo.write(183);
+    }
+    else if ((char)payload[0] == '0')
+    {
+      Serial.println("recived signal to close the door ");
+      PublisMqttString("home/door/0", "0");
+      myservo.write(18);
     }
   }
 }
